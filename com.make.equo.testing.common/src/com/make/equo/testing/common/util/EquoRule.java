cb10ad8eda75678cdddfdc6d6b2db80d51bd0c91
make2019 <jfferreira1993@gmail.com>
2020-04-16 14:00:19 -0300

    A more specific EclipseContext has been builded, allowing the execution of Eclipse4 Commands in Plugin Tests

--- com.make.equo.testing.common/src/com/make/equo/testing/common/util/EquoRule.java
+++ com.make.equo.testing.common/src/com/make/equo/testing/common/util/EquoRule.java
@@ -1,17 +1,26 @@
 package com.make.equo.testing.common.util;
 
+import static org.mockito.Mockito.mock;
+
 import org.eclipse.core.commands.CommandManager;
 import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
 import org.eclipse.e4.core.commands.internal.CommandServiceImpl;
+import org.eclipse.e4.core.commands.internal.HandlerServiceImpl;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.EclipseContextFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.services.contributions.IContributionFactory;
+import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.core.services.log.Logger;
 import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.e4.ui.internal.workbench.ModelServiceImpl;
+import org.eclipse.e4.ui.internal.workbench.ReflectionContributionFactory;
+import org.eclipse.e4.ui.internal.workbench.addons.CommandProcessingAddon;
+import org.eclipse.e4.ui.internal.workbench.addons.HandlerProcessingAddon;
 import org.eclipse.e4.ui.internal.workbench.swt.ResourceUtility;
+import org.eclipse.e4.ui.model.application.MAddon;
 import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.MApplicationFactory;
 import org.eclipse.e4.ui.workbench.IResourceUtilities;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.swt.SWT;
@@ -21,7 +30,6 @@ import org.eclipse.swt.widgets.Shell;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
-import org.mockito.Mockito;
 import org.osgi.framework.FrameworkUtil;
 
 import com.make.equo.renderers.EclipseWebRendererFactory;
@@ -35,6 +43,12 @@ public class EquoRule implements TestRule {
 	
 	private EclipseWebRendererFactory rendererFactory;
 
+	private CommandProcessingAddon commandAddon;
+
+	private HandlerProcessingAddon handlerAddon;
+
+	private ModelServiceImpl modelService;
+	
 	public EquoRule(Object equoTestingInjector) {
 		this.caseTest = equoTestingInjector;
 	}
@@ -63,40 +77,72 @@ public class EquoRule implements TestRule {
 	}
 
 	@SuppressWarnings({ "rawtypes", "unused" })
-	public EquoRule withApplicationContext() {
+	public EquoRule withApplicationContext(MApplication app) {
 		rendererFactory = new EclipseWebRendererFactory();
 		eclipseContext = EclipseContextFactory
 		.getServiceContext(FrameworkUtil.getBundle(this.getClass()).getBundleContext());
 		
 		rendererFactory.init(eclipseContext);
 	
+		IContributionFactory factory = new ReflectionContributionFactory();
+		eclipseContext.set(IContributionFactory.class, factory);
+
 		IResourceUtilities resources = new ResourceUtility();
 		eclipseContext.set(IResourceUtilities.class, resources);
 			
-		Logger logger = Mockito.mock(Logger.class);
+		Logger logger = mock(Logger.class);
 
 		eclipseContext.set(Logger.class, logger);
 		
-		MApplication app = MApplicationFactory.INSTANCE.createApplication();
 		app.setContext(eclipseContext);
 		eclipseContext.set(MApplication.class, app);
 		
-		EModelService modelService = new ModelServiceImpl(eclipseContext);
+		modelService = new ModelServiceImpl(eclipseContext);
 		eclipseContext.set(EModelService.class, modelService);
 		
-		CommandManager commandManager = new CommandManager();
-		
 		CommandServiceImpl commandService = new CommandServiceImpl();
+		CommandManager commandManager = new CommandManager();
 		commandService.setManager(commandManager);
 		eclipseContext.set(ECommandService.class, commandService);
-		
-		UISynchronize sync = Mockito.mock(UISynchronize.class);
 				
+		Display display = new Display();
+		UISynchronize sync =  new UISynchronize() {
+
+			@Override
+			public void syncExec(Runnable runnable) {
+					runnable.run();
+			}
+
+			@Override
+			public void asyncExec(Runnable runnable) {
+					runnable.run();
+				
+			}
+		};		
 		eclipseContext.set(UISynchronize.class, sync);
 		
-		Display d = new Display();
-		Shell shell = new Shell(d);
+		IEventBroker eventBroker = mock(IEventBroker.class);
+		eclipseContext.set(IEventBroker.class, eventBroker);
+		
+
+		EHandlerService handlerService = new  HandlerServiceImpl();
+		eclipseContext.set(EHandlerService.class, handlerService);
+		
+		Shell shell = new Shell(display);
 		Composite parent = new Composite(shell,SWT.NONE);
+		
+		IEclipseContext addonStaticContext = EclipseContextFactory.create();
+		for (MAddon addon : app.getAddons()) {
+			addonStaticContext.set(MAddon.class, addon);
+			Object obj = factory.create(addon.getContributionURI(), eclipseContext, addonStaticContext);
+			addon.setObject(obj);
+			if (addon.getElementId().equals("org.eclipse.e4.ui.workbench.commands.model")) {
+				commandAddon = (CommandProcessingAddon) obj;
+			}
+			if (addon.getElementId().equals("org.eclipse.e4.ui.workbench.handler.model")) {
+				handlerAddon = (HandlerProcessingAddon) obj;
+			}
+		}
 		
 		return this;
 	}
@@ -109,5 +155,16 @@ public class EquoRule implements TestRule {
 		return rendererFactory;
 	}
 
+	public CommandProcessingAddon getCommandAddon() {
+		return commandAddon;
+	}
+
+	public HandlerProcessingAddon getHandlerAddon() {
+		return handlerAddon;
+	}
+
+	public ModelServiceImpl getModelService() {
+		return modelService;
+	}
 	
 }
